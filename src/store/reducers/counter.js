401115de0819a6499df3074817b3d5e8861050b4
tushardahiya <tushardahiya84@gmail.com>
2020-12-30 22:38:08 +0530

    added a utility function to streamline reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,34 +1,61 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     counter :0
 }
 
 const reducer = (state= initialState , action) => {
+
+    // this is the shorter streamlined way where i use a utility function in the utility.js file
+    //  which deos all the spreading of the  updated state in the old state and recieves 2 parameters -> the old state and the values of be changes
+    // which can be seen here. TO SEE THE PREVIOUS WAY CHECK OUT THE COMMENTED ORIGINAL WAY BELOW
+
     switch(action.type) {
+
         case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter:state.counter+1
-            }
+            return updateObject(state , {counter:state.counter+1} )
+
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter:state.counter-1
-            }
+            return updateObject(state , {counter:state.counter-1} )
+
         case actionTypes.ADD:
-            return{
-                ...state,
-                counter:state.counter+action.value
-            }
+            return updateObject(state , {counter:state.counter+action.value} )
+
         case actionTypes.SUBTRACT:
-            return{
-                ...state,
-                counter:state.counter-action.value
-            }
+            return updateObject(state , {counter:state.counter-action.value} )
+
         default:
             return state;
+            
     }
+
+    // OLD WAY OF USING THE ACTIONTYPES WITHOUT IMPLEMENTING THE UTILITY.JS FUNCTION THAT STREAMLINED THE REDUCER FILE.....
+    
+    // switch(action.type) {
+    //     case actionTypes.INCREMENT:
+    //         return {
+    //             ...state,
+    //             counter:state.counter+1
+    //         }
+    //     case actionTypes.DECREMENT:
+    //         return {
+    //             ...state,
+    //             counter:state.counter-1
+    //         }
+    //     case actionTypes.ADD:
+    //         return{
+    //             ...state,
+    //             counter:state.counter+action.value
+    //         }
+    //     case actionTypes.SUBTRACT:
+    //         return{
+    //             ...state,
+    //             counter:state.counter-action.value
+    //         }
+    //     default:
+    //         return state;
+    // }
 }
 
 export default reducer;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results:[]
@@ -6,17 +7,14 @@ const initialState = {
 
 const reducer = (state= initialState , action) => {
     switch(action.type) {
+
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results:state.results.concat({ id: new Date().getTime() , value:action.result })
-            }
+            return updateObject(state, {results:state.results.concat({ id: new Date().getTime() , value:action.result })} )
+
         case actionTypes.DELETE_RESULT:
             const updatedResults = state.results.filter( result => result.id !== action.resultElementId )
-            return {
-                ...state,
-                results:updatedResults
-            }
+            return updateObject(state , {results:updatedResults} )
+            
         default:
             return state;
     }
