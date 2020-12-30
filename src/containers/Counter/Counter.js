732022218c17b6983bb05817ba8c57f5cf032cec
tushardahiya <tushardahiya84@gmail.com>
2020-12-30 22:14:33 +0530

    removed missing favicon error in console + added default case in Counter + added some comments

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -24,6 +24,8 @@ class Counter extends Component {
             case 'sub':
                 this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
                 break;
+            default :
+            break;
         }
     }
 
--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,13 +1,14 @@
 import * as actionTypes from './actionTypes';
 
 
-export const saveResult = (result) => {
+export const saveResult = (result) => { //will pass this const to storeResult which is using thunk to handle async action
     return {
         type:actionTypes.STORE_RESULT,
         result:result
     };
 }
 
+//using thunk middleware to handle async actions below
 export const storeResult = (result) => {
     return dispatch => {
        setTimeout( () => {
